deb5d2db6cb5cb88d3afe5059a6d74afdb6081a7
hrissup <hrish.ucan@gmail.com>
2026-03-30 04:54:11 +0530

    Added some new comments for later

--- src/main/DashboardUI.java
+++ src/main/DashboardUI.java
@@ -1,6 +1,7 @@
 package com.corepulse;
 
 import com.corepulse.PulseEngine.HardwareSnapshot;
+// Importing all the UI elements
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
@@ -13,14 +14,16 @@ import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class DashboardUI extends Application {
+public class DashboardUI extends Application { // MAIN CLASS [Standard JAvaFX graphical application]
 
-    private static final int MAX_DATA_POINTS = 60;
+    private static final int MAX_DATA_POINTS = 60; // Active tick limit [on Graph]
 
     
-    private XYChart.Series<Number, Number> cpuSeries;
+    private XYChart.Series<Number, Number> cpuSeries; // Container 1
 
-    private XYChart.Series<Number, Number> ramSeries;
+    private XYChart.Series<Number, Number> ramSeries; // Container 2
+
+// Variables for holding UI Text Elements 
 
     private Label cpuLabel;
 
@@ -28,36 +31,43 @@ public class DashboardUI extends Application {
 
     private Label uptimeLabel;
 
-    private PulseEngine pulseEngine;
+    private PulseEngine pulseEngine; // Reference for backend engine
 
-    private int tickCount = 0;
+    private int tickCount = 0; // [X-Axis : Time]
 
-    @Override
+    
+// UI Begins
+
     public void start(Stage stage) {
 
-        pulseEngine = new PulseEngine();
+        pulseEngine = new PulseEngine(); // Backend Engine
+
+        // Whenever pulseEngine emits a pulse, call onNewSnapshot()
         pulseEngine.setOnPulse(this::onNewSnapshot);
 
         NumberAxis cpuXAxis = createTimeAxis();
-        NumberAxis cpuYAxis = new NumberAxis(0, 100, 10);
+        NumberAxis cpuYAxis = new NumberAxis(0, 100, 10); // Hardcoded [0-100]
         cpuYAxis.setLabel("CPU (%)");
         cpuYAxis.setAutoRanging(false);
 
         LineChart<Number, Number> cpuChart = new LineChart<>(cpuXAxis, cpuYAxis);
         cpuChart.setTitle("CPU Usage");
+
+// Turning off chart animations and symbols for a cleaner look
+
         cpuChart.setAnimated(false);
         cpuChart.setCreateSymbols(false);
         cpuChart.setLegendVisible(false);
 
         cpuSeries = new XYChart.Series<>();
         cpuChart.getData().add(cpuSeries);
-        VBox.setVgrow(cpuChart, Priority.ALWAYS);
+        VBox.setVgrow(cpuChart, Priority.ALWAYS); // If window is resized, chart should stretch to fill the available vertical space evenly
 
         NumberAxis ramXAxis = createTimeAxis();
         NumberAxis ramYAxis = new NumberAxis();
         ramYAxis.setLabel("RAM (GB)");
 
-        ramYAxis.setAutoRanging(true);
+        ramYAxis.setAutoRanging(true); //Ram varies system to system
 
         LineChart<Number, Number> ramChart = new LineChart<>(ramXAxis, ramYAxis);
         ramChart.setTitle("RAM Usage");
@@ -69,13 +79,19 @@ public class DashboardUI extends Application {
         ramChart.getData().add(ramSeries);
         VBox.setVgrow(ramChart, Priority.ALWAYS);
 
+// Initializing Labels
+
         cpuLabel    = styledLabel("CPU: --",     15, true);
         ramLabel    = styledLabel("RAM: --",     15, true);
         uptimeLabel = styledLabel("Uptime: --",  13, false);
 
+// Horizontal arranging with 30-pixels spacesz
+
         HBox statsBar = new HBox(30, cpuLabel, ramLabel, uptimeLabel);
         statsBar.setPadding(new Insets(8, 16, 8, 16));
         statsBar.setStyle("-fx-background-color: #181825;");
+
+// Vertical Arranging [MASTER CONTAINER for the window]
 
         VBox root = new VBox(0, statsBar, cpuChart, ramChart);
         root.setStyle("-fx-background-color: #1e1e2e;");
@@ -85,10 +101,12 @@ public class DashboardUI extends Application {
         stage.setScene(scene);
         stage.show();
 
+// UI is ready -> Call Backend Engine
+
         pulseEngine.start();
     }
-    @Override
-    public void stop() {
+    
+    public void stop() { // Shuts down pulseEngine
         if(pulseEngine != null) {
             pulseEngine.stop();
         }
